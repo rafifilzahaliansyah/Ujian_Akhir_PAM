5efbba660518b7b94cd22311e4d11e9e062e5d16
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 00:27:27 +0700

    membuat Uistate

---
+++ app/src/main/java/com/example/ujian_akhir_pam/ui/Uistate.kt
@@ -0,0 +1,49 @@
+package com.example.ujian_akhir_pam.ui
+
+import com.example.ujian_akhir_pam.model.Kontak
+
+data class UIStateKontak(
+    val detailKontak: DetailKontak= DetailKontak(),
+)
+
+data class  DetailKontak(
+    val id: String = "",
+    val nama : String= "",
+    val alamat : String= "",
+    val telpon : String= "",
+    val tanggal: String= "",
+    val jam : String= "",
+)
+
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya*/
+fun DetailKontak.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telepon = telpon,
+    tanggal = tanggal,
+    jam = jam
+)
+
+data class DetailUIState(
+    val addEvent: DetailKontak = DetailKontak(),
+)
+
+fun Kontak.toDetailKontak(): DetailKontak =
+    DetailKontak(
+        id = id,
+        nama = nama,
+        alamat = alamat,
+        telpon = telepon,
+        tanggal = tanggal,
+        jam = jam
+    )
+
+fun Kontak.toUiStateKontak(): UIStateKontak = UIStateKontak(
+    detailKontak = this.toDetailKontak()
+)
+
+data class HomeUIState(
+    val listKontak: List<Kontak> = listOf(),
+    val dataLength: Int = 0
+)
