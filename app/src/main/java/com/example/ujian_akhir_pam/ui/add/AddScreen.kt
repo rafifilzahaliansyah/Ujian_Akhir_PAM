03b4bab903c788f57903cfb85a32c447fa3dcebc
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 14:02:49 +0700

    membuat AddScreen

---
+++ app/src/main/java/com/example/ujian_akhir_pam/ui/add/AddScreen.kt
@@ -0,0 +1,139 @@
+package com.example.ujian_akhir_pam.ui.add
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ujian_akhir_pam.R
+import com.example.ujian_akhir_pam.navigation.DestinasiNavigasi
+import com.example.ujian_akhir_pam.ui.DetailKontak
+import com.example.ujian_akhir_pam.ui.KontakTopAppBar
+import com.example.ujian_akhir_pam.ui.PenyediaViewModel
+import com.example.ujian_akhir_pam.ui.UIStateKontak
+import kotlinx.coroutines.launch
+
+object DestinasiKontak : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: AddViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            KontakTopAppBar(
+                title = DestinasiKontak.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+
+        AddKontakBody(
+            uiStateKontak = viewModel.uiStateKontak,
+            onKontakValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun AddKontakBody(
+    uiStateKontak: UIStateKontak,
+    onKontakValueChange: (DetailKontak) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        FormInput(
+            detailKontak = uiStateKontak.detailKontak,
+            onValueChange = onKontakValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    detailKontak: DetailKontak,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailKontak) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailKontak.nama,
+            onValueChange = {onValueChange(detailKontak.copy(nama=it)) },
+            label = { Text(stringResource(R.string.nama)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailKontak.alamat,
+            onValueChange = {onValueChange(detailKontak.copy(alamat=it)) },
+            label = { Text(stringResource(R.string.waktupenyewaan)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailKontak.telpon,
+            onValueChange = {onValueChange(detailKontak.copy(telpon=it)) },
+            label = { Text(stringResource(R.string.telpon)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+
+    }
+}
