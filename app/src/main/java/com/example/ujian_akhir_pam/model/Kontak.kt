acb5d921c2cc39af0eaf0737c09d63b10f6b5de6
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 14:14:08 +0700

    update class kontak

--- app/src/main/java/com/example/ujian_akhir_pam/model/Kontak.kt
+++ app/src/main/java/com/example/ujian_akhir_pam/model/Kontak.kt
@@ -3,8 +3,10 @@ package com.example.ujian_akhir_pam.model
 data class Kontak(
     val id: String,
     val nama: String,
-    val alamat: String,
+    val waktupenyewaan: String,
     val telepon: String,
+    val tanggal: String,
+    val jam:String
 ){
-    constructor(): this("","","","")
+    constructor(): this("","","","", "", "")
 }
