064dc9c30c65afe274b9fef1d9d03ce95cf18f8c
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 13:57:17 +0700

    membuat KontakApplication

--- app/src/main/java/com/example/ujian_akhir_pam/KontakApplication.kt
+++ app/src/main/java/com/example/ujian_akhir_pam/KontakApplication.kt
@@ -4,11 +4,12 @@ import android.app.Application
 import com.example.ujian_akhir_pam.data.AppContainer
 import com.example.ujian_akhir_pam.data.KontakContainer
 
-class KontakApplication:Application() {
+class KontakApplication: Application() {
     lateinit var container: AppContainer
 
     override fun onCreate() {
         super.onCreate()
+
         container = KontakContainer()
     }
 }
