c30e578a139c07ce1e0e1b38be2bce6265e6657b
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 14:46:19 +0700

    bikin function SearchBar

--- app/src/main/java/com/example/ujian_akhir_pam/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/ujian_akhir_pam/ui/home/HomeScreen.kt
@@ -12,15 +12,18 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -28,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -176,4 +180,27 @@ fun DataKontak(
             )
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    searchQuery: String,  // Tambahkan parameter searchQuery
+    modifier: Modifier = Modifier,
+    onSearchQueryChanged: (String) -> Unit,
+    onSearchClear: () -> Unit
+) {
+    TextField(
+        value = searchQuery,
+        onValueChange = { onSearchQueryChanged(it) },
+        placeholder = { Text(text = stringResource(R.string.search)) },
+        trailingIcon = {
+            if (searchQuery.isNotEmpty()) {
+                IconButton(onClick = { onSearchClear() }) {
+                    Icon(imageVector = Icons.Default.Clear, contentDescription = null)
+                }
+            }
+        },
+        modifier = modifier
+    )
 }
