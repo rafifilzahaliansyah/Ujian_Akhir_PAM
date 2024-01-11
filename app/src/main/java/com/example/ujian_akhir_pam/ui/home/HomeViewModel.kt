ea84f95056830b394511a6d19b49dc444e0de81c
Rafi Filzah Aliansyah <rafi.filzah.ft21@mail.umy.ac.id>
2024-01-11 15:05:58 +0700

    Update HomeViewModel

--- app/src/main/java/com/example/ujian_akhir_pam/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/ujian_akhir_pam/ui/home/HomeViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ujian_akhir_pam.data.KontakRepository
 import com.example.ujian_akhir_pam.model.Kontak
-import com.example.ujian_akhir_pam.ui.HomeUIState
+
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
@@ -23,16 +25,32 @@ class HomeViewModel(private val kontakRepository: KontakRepository) : ViewModel(
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> get() = _searchQuery
 
-    val homeUIState: StateFlow<HomeUIState> = kontakRepository.getAll()
+
+    val homeUiState: StateFlow<HomeUiState> = kontakRepository.getAll()
         .filterNotNull()
-        .map {
-            HomeUIState (listKontak = it.toList(), it.size ) }
+        .map { kontakList ->
+            // Filter siswaList based on the search query
+            val filteredList = kontakList.filter { kontak ->
+                kontak.nama.contains(_searchQuery.value, ignoreCase = true) ||
+                        kontak.waktupenyewaan.contains(_searchQuery.value, ignoreCase = true) ||
+                        kontak.telepon.contains(_searchQuery.value, ignoreCase = true)
+            }
+            HomeUiState(listKontak = filteredList)
+        }
         .stateIn(
             scope = viewModelScope,
             started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
-            initialValue = HomeUIState()
-
+            initialValue = HomeUiState()
         )
+    fun setSearchQuery(query: String) {
+        _searchQuery.value = query
+    }
 
 }
+
+data class HomeUiState(
+    val listKontak: List<Kontak> = listOf()
+)
